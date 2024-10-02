e0e3801db41c54f5288e5da98336bea682037af9
Pawanpawar <pawan.p7700@gmail.com>
2024-10-02 18:30:41 +0530

    db.sqlite file and allow origin

--- Mini_Project/Resturant_Project/Resturant_Project/settings.py
+++ Mini_Project/Resturant_Project/Resturant_Project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t_!n%3ph(-bpkd2+z2!zujx+6r_x%&3%allmra#8#x0v*iogzf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Resturant_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Template'],
+        'DIRS': [BASE_DIR / 'Template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,10 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_ROOT = BASE_DIR/'static'
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR / "static")]
+ #STATICFILES_DIRS = [os.path.join(BASE_DIR / "static")]
 
 MEDIA_URL = 'media/'
 
